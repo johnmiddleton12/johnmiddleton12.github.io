d79a87ed327890b6c446def46cecb15e206787d8
johnmiddleton12 <johnathanmiddleton1@gmail.com>
2020-12-24 19:58:36 -0500

    no boot test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// let mainNav = document.getElementById('js-menu');
+// let navBarToggle = document.getElementById('js-navbar-toggle');
+
+// navBarToggle.addEventListener('click', function () {
+//   mainNav.classList.toggle('active');
+// });
 
 // Animated Console Text
 
